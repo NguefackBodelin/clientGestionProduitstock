c936a5148947288080f61ecb306de1d002625955
NguefackBodelin <60115415+NguefackBodelin@users.noreply.github.com>
2020-09-15 16:24:18 +0200

    ajouter le module de routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ProduitComponent } from './produit/produit.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  {path:'produit', component:ProduitComponent },
+  {path:'dashboard', component:DashboardComponent },// utile sulle slidebar.html a traver router-outlet dans content.component.html
+  {path:'', redirectTo:'/dashboard', pathMatch:'full'} // par defaut il me met le dashboard
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes,{enableTracing:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
